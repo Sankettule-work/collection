ee3fa8deaa47abcca1727b603de72b9050f7a0f7
Sankettule-work <sanketwarud@gmail.com>
2025-01-11 12:16:20 +0530

    Add: My branch Sanket

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi,this is Sanket tule");
+        System.out.println("Hello,this is Sanket tule.I am doing to create conflict ");
         System.out.println("This is from sanket branch");
     }
 }
